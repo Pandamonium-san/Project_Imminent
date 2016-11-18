78b3ce84e50d89ab647d46ef5e38411f4933f9c0
Stiffrock <christofer.malmberg@gmail.com>
2016-11-18 15:15:32 +0100

    Adde checkpoint system

---
+++ Source/Project_Imminent/Checkpoint.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Checkpoint.generated.h"
+
+UCLASS()
+class PROJECT_IMMINENT_API ACheckpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACheckpoint();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CheckpointID)
+	FString id;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision)
+		UBoxComponent* CheckPointHitBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Position)
+		FVector CheckpointPosition;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Position)
+	FRotator CheckpointRotation;
+
+	USceneComponent* RootComponent;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
